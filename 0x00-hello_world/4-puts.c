a19335e9988daf8fe1f8698c02fd29e6067f8e95
Nenkkyoung <ynenzy@gmail.com>
2022-06-30 07:01:09 -0700

    c hello world is completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"P rogramming is like building a multilingual puzzle");
 return (0);
+}
