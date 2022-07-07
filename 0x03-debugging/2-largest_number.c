44a623ad41772869a773a0402a66c6b1e4c409c5
Nenkkyoung <ynenzy@gmail.com>
2022-07-07 02:45:39 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -25,6 +25,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-		return (largest);
+	return (largest);
 }
-	
