a06debdbbe14d6112baeea1adcfbeadaa8db1573
Nenkkyoung <ynenzy@gmail.com>
2022-07-15 06:35:16 -0700

    task 9

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,11 +2,10 @@
 #define MAIN_H
 #include <stdio.h>
 /**
-
+ * _putchar prototypes()
  * void prototypes()
  * int prototypes()
  * char prototypes()
- * _putchar prototypes()
  */
 void reverse_array(int *a, int n);
 void print_number(int n);
